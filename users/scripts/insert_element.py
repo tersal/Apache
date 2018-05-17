2b7c390d6e1d19bc5942ba19ae22afb5288e7b36
tersal <moises.tersal@gmail.com>
2018-05-16 23:14:20 -0500

    Added communication with the database
    More simple code, very error prone, baby steps.
    
    I will keep working on this, there is still a lot of things to do but first I wanted to test
    that everything is working.

--- users/scripts/insert_element.py
+++ users/scripts/insert_element.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
 
 import cgi
+import pymysql
+import json
+
 form = cgi.FieldStorage()
 
 lastName = form.getvalue("lastName")
 firstName = form.getvalue("firstName")
 
-print("Content-Type: text/html; charset=utf-8\n\n")
-print("<html>")
-print("<head>")
-print("<title>Get Information</title>")
-print("</head>")
-print("<body>")
-print("<h1>Name is: {} {}</h1>".format(firstName, lastName))
-print("</body>")
-print("</html>")
+print("Content-Type: application/json")
+print("\n")
+print("\n")
+
+conn = pymysql.connect(
+	db='example',
+	user='root',
+	passwd='KittyKat',
+	host='localhost')
+
+curs = conn.cursor()
+
+curs.execute("INSERT INTO persons (FirstName, LastName) VALUES('{}', '{}');".format(firstName, lastName))
+conn.commit()
+
+result = {}
+result['success'] = True
+result['message'] = "Inserte into the database, maybe"
+
+print(json.dumps(result, indent=1))
+print("\n")
